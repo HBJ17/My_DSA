54dd346f0eefc7b7c2dcfb3cf6d2cae5a7021c9e
HBJ17 <hbj17672007@gmail.com>
2025-12-16 16:42:10 +0530

    learnt linear search

---
+++ Linear_Search.py
@@ -0,0 +1,13 @@
+lst = [1,32,45,65,2,4,98,78,34,80,32,68,32,80,532,12,4,678,9,42,45,990,55]
+
+n = int(input("Enter Target number = "))
+
+found = False
+for i in lst:
+    if i == n:
+        found = True
+        break
+
+print(f"Found at position {lst.index(n)}" if found else "Not found")
+
+
